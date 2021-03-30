0d4625811f34563a1c2866eac8e5b2030eb66e15
Bingcheng Wang <470797339@qq.com>
2021-03-30 14:14:09 +0800

    first commit

---
+++ build/CMakeFiles/sub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/sub"
+  "../bin/sub.pdb"
+  "CMakeFiles/sub.dir/src/sub.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
